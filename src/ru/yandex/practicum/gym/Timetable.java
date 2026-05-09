933c1fa5c3aad7deca767825a8900ad29a549ea5
Plisha <mm3422kk@outlook.com>
2026-05-09 21:24:33 +0500

    добавил 3 метода

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -1,20 +1,51 @@
 package ru.yandex.practicum.gym;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private HashMap<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        //сохраняем занятие в расписании
+        DayOfWeek dayOfWeek = trainingSession.getDayOfWeek();
+        TimeOfDay timeOfDay = trainingSession.getTimeOfDay();
+        if (!timetable.containsKey(dayOfWeek)) {
+            TreeMap<TimeOfDay, List<TrainingSession>> day = new TreeMap<>();
+            timetable.put(dayOfWeek, day);
+        }
+        TreeMap<TimeOfDay, List<TrainingSession>> day = timetable.get(dayOfWeek);
+        if (!day.containsKey(timeOfDay)) {
+            List<TrainingSession> sessionList = new ArrayList<>();
+            day.put(timeOfDay, sessionList);
+        }
+        List<TrainingSession> sessionList = day.get(timeOfDay);
+        sessionList.add(trainingSession);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        if (!timetable.containsKey(dayOfWeek)) {
+            return new ArrayList<>();
+        }
+        TreeMap<TimeOfDay, List<TrainingSession>> day = timetable.get(dayOfWeek);
+        List<TrainingSession> sessions = new ArrayList<>();
+        for (TimeOfDay time : day.navigableKeySet()) {
+            sessions.addAll(day.get(time));
+        }
+        return sessions;
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        if (!timetable.containsKey(dayOfWeek)) {
+            return new ArrayList<>();
+        }
+        TreeMap<TimeOfDay, List<TrainingSession>> day = timetable.get(dayOfWeek);
+        if (day.containsKey(timeOfDay)) {
+            return day.get(timeOfDay);
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
